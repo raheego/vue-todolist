79427e111840d856b5ebe562d7fda7b09081c6ee
raheego <54056684+raheego@users.noreply.github.com>
2020-10-02 15:59:28 +0900

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
+<!-- /src/App.vue -->
+
 <template>
-  <div id="app">
-  </div>
+    <div id="app">
+      <TodoHeader></TodoHeader>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+      <TodoFooter></TodoFooter>
+    </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter';
 
 export default {
+    components: {
+      'TodoHeader': TodoHeader,
+      'TodoInput': TodoInput,
+      'TodoList': TodoList,
+      'TodoFooter': TodoFooter
+    }
 }
 </script>
 
 <style>
+
 </style>
